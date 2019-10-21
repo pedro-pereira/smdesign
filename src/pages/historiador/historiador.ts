a9252579c4770e03f4f398cdd1f9acd2c4ba1a72
pedro-pereira <pedrohenripereira@gmail.com>
2019-10-20 23:39:14 -0300

    Implementacao de botoes
    Implementacao de botoes que trocam css (cor, fonte, imagem, layout) e redesenho de grid

--- src/pages/historiador/historiador.ts
+++ src/pages/historiador/historiador.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-// import {  FabContainer } from 'ionic-angular';
-
 @Component({
   selector: 'page-historiador',
   templateUrl: 'historiador.html'
@@ -15,27 +13,145 @@ export class HistoriadorPage {
   escola1Cor: HTMLElement;
   escola1Acessorios: HTMLElement;
 
+  caracteristicasTextuais = ['textoCorNeutro', 'textoFonteNeutro'];
+  caracteristicasImageticas = ['imagemNeutro', 'layoutNeutro'];
+
   constructor(public navCtrl: NavController) {
   }
 
-  recuperaElementosDaPagina() {
-    this.escola1Tipografia  = document.getElementById("escola1-tipografia");
-    this.escola1Layout      = document.getElementById("escola1-layout");
-    this.escola1Cor         = document.getElementById("escola1-cor");
-    this.escola1Acessorios  = document.getElementById("escola1-acessorios");
+  acaoBotaoX() {
+    alert('Botão X');
   }
 
-  destacaBotao(fonte: String) {
-    
-    this.recuperaElementosDaPagina();
-    
-    /*
-    if(fonte === 'escola1-tipografia') {
-      this.escola1Tipografia.classList.toggle('botaoNormal');
-      this.escola1Layout.classList.toggle('botaoTransparente');
-      this.escola1Cor.classList.toggle('botaoTransparente');
-      this.escola1Acessorios.classList.toggle('botaoTransparente');
+  acaoBotaoY() {
+    alert('Botão Y');
+  }
+
+  // -------------- ART DECO --------------
+  exibeDialogoArtDeco() {
+    var balaoDeDialogo = document.getElementById('balaoDeDialogo');
+    balaoDeDialogo.className = "balaoDeDialogoArtDeco";
+  }
+
+  aplicaCorArtDeco() {
+    this.caracteristicasTextuais[0] = "textoCorArtDeco";
+    var todosOsTextos = document.getElementsByClassName('texto');
+    for (var i = 0; i < todosOsTextos.length; i++) {
+      todosOsTextos[i].className = "texto";
+      for (var j = 0; j < this.caracteristicasTextuais.length; j++) {
+        todosOsTextos[i].className += " " + this.caracteristicasTextuais[j];
+      }
     }
+
+    var todosOsBotoesDeCor = document.getElementsByClassName('botaoDeCor');
+    for (i = 0; i < todosOsBotoesDeCor.length; i++) {
+      todosOsBotoesDeCor[i].classList.add('opacidadeBotao50');
+      console.log(todosOsBotoesDeCor[i].className);
+      console.log(todosOsBotoesDeCor[i].classList);
+
+    }
+    /*
+    console.log('aplicaCorArtDeco');
+    console.log(this.caracteristicasTextuais);
+    console.log(todosOsBotoesDeCor);
     */
   }
+
+  aplicaFonteArtDeco() {
+    this.caracteristicasTextuais[1] = "textoFonteArtDeco";
+    var todosOsTextos = document.getElementsByClassName('texto');
+    for (var i = 0; i < todosOsTextos.length; i++) {
+      todosOsTextos[i].className = "texto";
+      for (var j = 0; j < this.caracteristicasTextuais.length; j++) {
+        todosOsTextos[i].className += " " + this.caracteristicasTextuais[j];
+      }
+    }
+    /*
+    console.log('aplicaFonteArtDeco');
+    console.log(this.caracteristicasTextuais);
+    */
+  }
+
+  aplicaImagemArtDeco() {
+    this.caracteristicasImageticas[0] = "imagemArtDeco";
+    var todasAsImagens = document.getElementsByClassName('imagem');
+    for (var i = 0; i < todasAsImagens.length; i++) {
+      todasAsImagens[i].className = "imagem";
+      for (var j = 0; j < this.caracteristicasImageticas.length; j++) {
+        todasAsImagens[i].className += " " + this.caracteristicasImageticas[j];
+      }
+    }
+    /*
+    console.log('aplicaImagemArtDeco');
+    console.log(this.caracteristicasImageticas);
+    */
+  }
+
+  aplicaLayoutArtDeco() {
+    this.caracteristicasImageticas[1] = "layoutArtDeco";
+    var todasAsImagens = document.getElementsByClassName('imagem');
+    for (var i = 0; i < todasAsImagens.length; i++) {
+      todasAsImagens[i].className = "imagem";
+      for (var j = 0; j < this.caracteristicasImageticas.length; j++) {
+        todasAsImagens[i].className += " " + this.caracteristicasImageticas[j];
+      }
+    }
+    /*
+    console.log('aplicaLayoutArtDeco');
+    console.log(this.caracteristicasImageticas);
+    */
+  }
+
+  // -------------- ART NOVEAU --------------
+  exibeDialogoArtNoveau() {
+    var balaoDeDialogo = document.getElementById('balaoDeDialogo');
+    balaoDeDialogo.className = "balaoDeDialogoArtNoveau";
+  }
+
+  aplicaCorArtNoveau() {
+    this.caracteristicasTextuais[0] = "textoCorArtNoveau";
+    var todosOsTextos = document.getElementsByClassName('texto');
+    for (var i = 0; i < todosOsTextos.length; i++) {
+      todosOsTextos[i].className = "texto";
+      for (var j = 0; j < this.caracteristicasTextuais.length; j++) {
+        todosOsTextos[i].className += " " + this.caracteristicasTextuais[j];
+      }
+    }
+    /*
+    console.log('aplicaCorArtNoveau');
+    console.log(this.caracteristicasTextuais);
+    console.log(todosOsBotoesDeCor);
+    */
+  }
+
+  aplicaFonteArtNoveau() {
+    this.caracteristicasTextuais[1] = "textoFonteArtNoveau";
+    var todosOsTextos = document.getElementsByClassName('texto');
+    for (var i = 0; i < todosOsTextos.length; i++) {
+      todosOsTextos[i].className = "texto";
+      for (var j = 0; j < this.caracteristicasTextuais.length; j++) {
+        todosOsTextos[i].className += " " + this.caracteristicasTextuais[j];
+      }
+    }
+    /*
+    console.log('aplicaFonteArtNoveau');
+    console.log(this.caracteristicasTextuais);
+    */
+  }
+
+  aplicaImagemArtNoveau() {
+    this.caracteristicasImageticas[0] = "imagemArtNoveau";
+    var todasAsImagens = document.getElementsByClassName('imagem');
+    for (var i = 0; i < todasAsImagens.length; i++) {
+      todasAsImagens[i].className = "imagem";
+      for (var j = 0; j < this.caracteristicasImageticas.length; j++) {
+        todasAsImagens[i].className += " " + this.caracteristicasImageticas[j];
+      }
+    }
+    /*
+    console.log('aplicaImagemArtNoveau');
+    console.log(this.caracteristicasImageticas);
+    */
+  }
+
 }
